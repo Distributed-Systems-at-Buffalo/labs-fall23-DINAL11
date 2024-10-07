45ad8339f01c94c301ea4b7c1996d5fdc28d957c
Dinal <79514410+DINAL11@users.noreply.github.com>
2024-10-07 11:18:26 -0400

    Update schedule.go

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -23,46 +23,7 @@ func (mr *Master) schedule(phase jobPhase) {
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 
 	// Initialize  number of tasks
-	x := 0
-	// Create a channel to receive task completion signals.
-	taskcomchannel := make(chan bool, ntasks)
-
-	for i := 0; i < ntasks; i++ {
-		go func(numtask int) {
-			for {
-				// Wait for a registered worker.
-				regworker := <-mr.registerChannel
-
-				//DoTaskArgs for task.
-				args := DoTaskArgs{
-					JobName:       mr.jobName,
-					File:          mr.files[numtask],
-					Phase:         phase,
-					TaskNumber:    numtask,
-					NumOtherPhase: nios,
-				}
-
-				// initiates a task on worker using an RPC
-				rpccall := call(regworker, "Worker.DoTask", &args, new(struct{}))
-				if rpccall {
-					// notify the channel of task completion.
-					taskcomchannel <- true
-
-					// return the worker by sending it to the register channel.
-					go func() {
-						mr.registerChannel <- regworker
-					}()
-					break
-				}
-			}
-		}(i)
-	}
-
-	// Wait for all tasks to complete.
-	for x < ntasks {
-		<-taskcomchannel
-		x++
-	}
+	
 
 	close(taskcomchannel)
 
